cd636d10134560b02a821bee1dc38b539f5915ce
Drowned-fish <867203437@qq.com>
2021-07-17 09:59:55 +0800

    feat: 添加generate方法

---
+++ src/utils/generate.ts
@@ -0,0 +1,18 @@
+import { TransForm } from "../type/interface";
+
+export const generate = (ast:TransForm[]) => {
+    const cssLines = ast.reduce((cssArr, {dels,selector, comment})=> {
+        if(comment) {
+            cssArr.push(comment);
+        }
+        else if(selector && dels) {
+            cssArr.push(selector + " {");
+            cssArr.push(dels.map((del) => ` ${del.prop}: ${del.value};`).join('\n'));
+            cssArr.push('}');
+        }
+
+        return cssArr;
+    },[] as string[]);
+
+    return cssLines.join('\n');
+}
